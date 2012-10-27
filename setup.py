f7e5f26c12000859b2efb5b3d3b16b9afb7eeb16
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-10-27 20:45:23 +0100

    installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'Onset detection',
+    version = '0.1',
+    packages = ['onsetdetection'],
+    author = 'Andreas Jansson',
+    author_email = 'andreas@jansson.me.uk',
+    description = ('Onset detection using high frequency content and hill climbing'),
+    license = 'GPL v3',
+    )
